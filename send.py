69baab9b6e4804c3d5424459015f10cbf5bef90d
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-25 11:45:46 +0530

    Create send.py

---
+++ send.py
@@ -0,0 +1,36 @@
+from pyrogram import Client, Filters,Emoji
+from pyrogram.errors import FloodWait
+import time
+from emoji import UNICODE_EMOJI
+app = Client("mnnnnnnn",488556,"c722b7aadbf8b72109b2f96f30974c6d")
+s = -1001146486274
+d = -1001378725482
+g = ["0","1","2","3","4","6","NEED","RUN","CATCH","DROP","BALL","HAWA","WD","WIDE","NB","PLAYING","OVER","WON","WIN","START","STOP","ME","+","XI","✔️","GANA","GAANA","TIE","WKT","WICKET","☄️"]
+@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
+def forward(client,Message):
+ r = s = False
+ words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+ for word in words:
+  if word.casefold() in Message.text.casefold():
+   return
+ for i in Message.text.split(' '):
+  if i in UNICODE_EMOJI:
+   r = True
+  for p in g:
+   if p.casefold() in Message.text.casefold():
+    s = True
+ if r or s:
+  if "🎾" in Message.text:
+   mes = client.send_message(d,"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾" + "</b>",parse_mode= "html")
+  else:
+   mes = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+  files = open("sure.txt" , "a")
+  files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
+  files.close()
+@app.on_message(Filters.command("cw"))
+def main(client, message):
+ with open("sure.txt" , "w") as files:
+  files.write("")
+  files.close()
+  message.reply("Done") 
+app.run()
